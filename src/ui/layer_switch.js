ff685898294a641edc3f0515d2def660265611d1
Mark Mathis <aliasmrchips@gmail.com>
2018-06-09 20:46:48 -0600

    add layer dialog

--- src/ui/layer_switch.js
+++ src/ui/layer_switch.js
@@ -1,19 +1,25 @@
 import React from "react";
-import L from "leaflet";
-import { layers } from "../layers";
 
-export default ({ layer, setLayer }) => (
-  <div className="flex">
-    {layers.map(({ id, title }) => (
-      <span
-        key={id}
-        onClick={() => setLayer(id)}
-        className={`db pointer bn pa2 nowrap outline-0 ${
-          layer === id ? "bg-yellow" : ""
-        }`}
-      >
-        {title}
-      </span>
-    ))}
-  </div>
-);
+class LayerSwitch extends React.Component {
+
+  render() {
+    const { layers, layer, setLayer } = this.props;
+    return (
+      <div className="flex">
+        {layers.map(({ id, title }) => (
+          <span
+            key={id}
+            onClick={() => setLayer(id)}
+            className={`db pointer bn pa2 nowrap outline-0 ${
+              layer === id ? "bg-yellow" : ""
+            }`}
+          >
+            {title}
+          </span>
+        ))}
+      </div>  
+    )
+  }
+}
+
+export default LayerSwitch;
